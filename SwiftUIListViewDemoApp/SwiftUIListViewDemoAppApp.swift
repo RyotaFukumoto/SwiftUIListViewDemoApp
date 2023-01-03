8056b4da0411e7517506d6c52b006779cc7d8791
ryota <k014c1106@it-neec.jp>
2023-01-03 17:28:28 +0900

    Initial Commit

---
+++ SwiftUIListViewDemoApp/SwiftUIListViewDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListViewDemoAppApp.swift
+//  SwiftUIListViewDemoApp
+//
+//  Created by ryota on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListViewDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
